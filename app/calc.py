71185c073c1acab99b140ebe030db6d48f56080b
std0002 <std93@hotmail.es>
2023-12-04 23:14:36 +0100

    up

---
+++ app/calc.py
@@ -0,0 +1,53 @@
+import app
+import math
+
+class InvalidPermissions(Exception):
+    pass
+
+
+class Calculator:
+    def add(self, x, y):
+        self.check_types(x, y)
+        return x + y
+
+    def substract(self, x, y):
+        self.check_types(x, y)
+        return x - y
+
+    def multiply(self, x, y):
+        if not app.util.validate_permissions(f"{x} * {y}", "user1"):
+            raise InvalidPermissions('User has no permissions')
+
+        self.check_types(x, y)
+        return x * y
+
+    def divide(self, x, y):
+        self.check_types(x, y)
+        if y == 0:
+            raise TypeError("Division by zero is not possible")
+
+        return x / y
+
+    def power(self, x, y):
+        self.check_types(x, y)
+        return x ** y
+    
+    def raiz(self, x):
+        return math.sqrt(x)
+
+    def logaritmo_base10(self, x):
+        return math.log10(x)
+    
+    def check_types(self, x, y):
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Parameters must be numbers")
+
+
+if __name__ == "__main__":  # pragma: no cover
+    calc = Calculator()
+    result = calc.add(2, 2)
+    result1 = calc.raiz(40)
+    resulta2 = calc.logaritmo_base10(100)
+    print(result)
+    print(result1)
+    print(resulta2)
---
+++ test/unit/prueba.py
@@ -0,0 +1,65 @@
+import http.client
+import os
+import unittest
+from urllib.request import urlopen
+
+import pytest
+
+BASE_URL = os.environ.get("BASE_URL")
+DEFAULT_TIMEOUT = 2  # in secs
+
+
+@pytest.mark.api
+class TestApi(unittest.TestCase):
+    def setUp(self):
+        self.assertIsNotNone(BASE_URL, "URL no configurada")
+        self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
+
+    def test_api_add(self):
+        url = f"{BASE_URL}/calc/add/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        
+    def test_api_substract(self):
+        url = f"{BASE_URL}/calc/substract/6/3"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/3/6"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/18/3"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    
+    def test_api_power(self):
+        url = f"{BASE_URL}/calc/power/2/4"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        
+    def test_api_logaritmo_base10(self):
+        url = f"{BASE_URL}/calc/logaritmo_base10/100"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    
+    def test_api_raiz(self):
+        url = f"{BASE_URL}/calc/raiz/64"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
